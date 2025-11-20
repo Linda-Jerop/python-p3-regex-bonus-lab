2759f5f715ea5b7aac19486e3b310deb8ee521c4
Linda <jeropkoimur@gmail.com>
2025-11-20 13:37:41 +0300

    RegEx Bonus-patterns

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,5 @@
 import re
 
-my_pattern = r""
+my_pattern = r"[A-Z][A-Za-z' ,]*today[A-Za-z' ,]*[.?]"  # Matches sentences with capitalized start, containing 'today', ending with . or ?
 my_regex = re.compile(my_pattern)
 
